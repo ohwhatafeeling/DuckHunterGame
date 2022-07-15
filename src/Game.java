e602f1d9cfb8b043a9af7d3624e0700bf66a0c34
Michael Macdonald <michaelmacdonald@Michaels-Air.modem>
2022-07-15 16:40:42 +0930

    Add sound effects

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,14 @@
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 public class Game extends JFrame implements MouseListener {
@@ -19,22 +26,42 @@ public class Game extends JFrame implements MouseListener {
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
+	
+	public void playSound(String fileName) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		File file = new File(fileName);
+		AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+		Clip clip = AudioSystem.getClip();
+		clip.open(ais);
+		clip.start();
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		Point point = e.getPoint();
-		if (panel.imageBounds.contains(point)) {
-			panel.score++;
-			panel.scoreLabel.setText("Score: " + panel.score);
-			System.out.println(panel.score);
-		}
+		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		Point point = e.getPoint();
+		if (panel.imageBounds.contains(point)) {
+			panel.score++;
+			panel.scoreLabel.setText("Score: " + panel.score);
+			try {
+				playSound("hit.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} else {
+			try {
+				playSound("miss.wav");
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 	}
 
 	@Override
