4467657c285ed7753376431e5541ebdf73ffef5d
Michael Macdonald <michaelmacdonald@Michaels-Air.modem>
2022-07-15 09:45:19 +0930

    Add ability to click image

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,57 @@
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements MouseListener {
+	GamePanel panel;
 	
 	Game() {
-		GamePanel panel = new GamePanel();
+		panel = new GamePanel();
+		panel.addMouseListener(this);
 		
 		this.setTitle("Duck Hunter");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.add(panel);
 		this.pack();
+		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		Point point = e.getPoint();
+		if (panel.imageBounds.contains(point)) {
+			panel.score++;
+			System.out.println(panel.score);
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,30 +1,53 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
-	final int PANEL_WIDTH = 700;
-	final int PANEL_HEIGHT = 500;
+	JPanel sky;
+	JPanel grass;
+	JLabel scoreLabel;
 	Image duck;
 	Timer timer;
+	final int PANEL_WIDTH = 700;
+	final int PANEL_HEIGHT = 500;
 	int xVelocity = 1;
 	int yVelocity = 1;
 	int x = 0;
 	int y = 0;
+	int score = 0;
+	Rectangle imageBounds;
 	
 	GamePanel() {
 		this.setPreferredSize(new Dimension(700, 500));
 		
+		sky = new JPanel();
+		sky.setPreferredSize(new Dimension(700, 300));
+		sky.setBackground(Color.BLUE);
+		
+		grass = new JPanel();
+		grass.setPreferredSize(new Dimension(700, 200));
+		grass.setBackground(Color.GREEN);
+		
+//		scoreLabel = new JLabel();
+		
 		duck = new ImageIcon("duck.png").getImage();
 		
-		timer = new Timer(5, this);
+		imageBounds = new Rectangle(x, y, duck.getWidth(null), duck.getHeight(null));
+		
+		this.add(sky);
+		this.add(grass);
+		
+		timer = new Timer(10, this);
 		timer.start();
 	}
 	
@@ -44,6 +67,7 @@ public class GamePanel extends JPanel implements ActionListener {
 			yVelocity = yVelocity * -1;
 		}
 		y = y + yVelocity;
+		imageBounds = new Rectangle(x, y, duck.getWidth(null), duck.getHeight(null));
 		repaint();
 	}
 
